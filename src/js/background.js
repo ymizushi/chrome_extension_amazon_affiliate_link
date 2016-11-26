a49355cfe28cfe7992faa37a70109de468e4585f
Hiroki Takayasu <hiroki.kana@gmail.com>
2016-11-26 15:21:08 +0900

    fixed option

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,7 @@
-$(function() {
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        alert(request);
         if (request.method == "getLocalStorage") {
             sendResponse({data: localStorage.getItem(request.key)});
         }else{
             sendResponse({});
         }
     });
-});
--- src/js/convert_amazon_link.js
+++ src/js/convert_amazon_link.js
@@ -1,30 +1,27 @@
 $(function() {
-    // TODO: !! not working !!
-    var affiliate_key_list = "";
     chrome.runtime.sendMessage({method : "getLocalStorage", key : "affiliate_key_list"}, function(response) {
-        affiliate_key_list = response.data;
-    });
+		var affiliate_key_list = response.data;
+		var affiliate_tags = ["hirokikana-22"];
+		if (affiliate_key_list) {
+			affiliate_tags = affiliate_key_list.replace(/ /g, "").split(",");
+		}
     
-    var affiliate_tags = ["hirokikana-22"];
-    if (affiliate_key_list) {
-        affiliate_tags = affiliate_key_list.replace(/ /g, "").split(",");
-    }
-    
-    $("a").each(function() {
-        if ($(this).attr('href').indexOf("amazon.co.jp") > -1) {
-            var affiliate_key_index = Math.floor(Math.random() * affiliate_tags.length);
-            var affiliate_key = "tag=" + affiliate_tags[affiliate_key_index];
-            var url = decodeURIComponent($(this).attr('href'));
-            var replace = "";
-            if(url.indexOf("tag=") > -1) {
-                replace = url.replace(/tag=[a-z-0-9]+/g, affiliate_key)
-            }else if (url.indexOf("?") > -1) {
-                replace = url + "&" + affiliate_key;
-            }else{
-                replace = url + "?" + affiliate_key;
-            }
-            $(this).attr('href', replace);
-        }
+		$("a").each(function() {
+			if ($(this).attr('href').indexOf("amazon.co.jp") > -1) {
+				var affiliate_key_index = Math.floor(Math.random() * affiliate_tags.length);
+				var affiliate_key = "tag=" + affiliate_tags[affiliate_key_index];
+				var url = decodeURIComponent($(this).attr('href'));
+				var replace = "";
+				if(url.indexOf("tag=") > -1) {
+					replace = url.replace(/tag=[a-z-0-9]+/g, affiliate_key)
+				}else if (url.indexOf("?") > -1) {
+					replace = url + "&" + affiliate_key;
+				}else{
+					replace = url + "?" + affiliate_key;
+				}
+				$(this).attr('href', replace);
+			}
+		});
     });
 });
 
